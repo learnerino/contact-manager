1a698c6a4c55e6d298b4d048a7d7fc7a31a19b88
learnerino <akarnicora+1@gmail.com>
2020-06-28 20:16:25 +0200

    Updates

---
+++ precache-manifest.cb9d8913198035ebe3be92ea2a776829.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56fc49a02d658b62cbd7c8c46bc0fbe0",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "e2dd772ec7af2756b6a4",
+    "url": "/contact-manager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "40e7357ca8bb65b0a36d",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "e2dd772ec7af2756b6a4",
+    "url": "/contact-manager/static/js/2.7b63b481.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact-manager/static/js/2.7b63b481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40e7357ca8bb65b0a36d",
+    "url": "/contact-manager/static/js/main.dd468b0f.chunk.js"
+  },
+  {
+    "revision": "8dc9346f41ed168d3261",
+    "url": "/contact-manager/static/js/runtime-main.2e7bbdb6.js"
+  }
+]);
